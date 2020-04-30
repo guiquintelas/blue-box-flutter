1a6805da50a5b2cc0466b68f6ce953e2a2c1df28
Guilherme Frota <guiquintelas96@gmail.com>
2020-04-30 18:44:26 -0300

    use Palatte helper class

---
+++ lib/palette.dart
@@ -0,0 +1,11 @@
+library paints;
+
+import 'dart:ui';
+
+import 'package:flame/palette.dart';
+
+class Palette {
+  static PaletteEntry white = BasicPalette.white;
+  static PaletteEntry black = BasicPalette.black;
+  static PaletteEntry blue = PaletteEntry(Color(0xFF0000ff));
+}
