13be4e3da150f1c293bbe8bdd7ab9bf51cd99f16
Guilherme Frota <guiquintelas96@gmail.com>
2020-05-06 13:08:36 -0300

    add init logic in game class

--- lib/blue_box_game.dart
+++ lib/blue_box_game.dart
@@ -6,13 +6,13 @@ import 'components/player.dart';
 
 class BlueBoxGame extends BaseGame with PanDetector {
   Player player;
-
-  BlueBoxGame() {
-    add(this.player = Player());
-  }
+  Size screenSize;
+  bool isInit = false;
 
   @override
   void onPanUpdate(DragUpdateDetails details) {
+    if (!isInit) return;
+
     super.onPanUpdate(details);
     this.player.onPanUpdate(details);
 
@@ -21,5 +21,24 @@ class BlueBoxGame extends BaseGame with PanDetector {
         (component as PanDetector).onPanUpdate(details);
       }
     }
+  }
+
+  void resize(Size size) {
+    super.resize(size);
+    screenSize = size;
+
+    init();
+    isInit = true;
+  }
+
+  void init() {
+    add(player = Player());
+  }
+
+  @override
+  void update(double t) {
+    if (!isInit) return;
+
+    super.update(t);
   }
 }
