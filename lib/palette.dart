a6fce4fdbdc483602bc06f7e305560838b4491b4
Guilherme Frota <guiquintelas96@gmail.com>
2020-05-06 17:16:06 -0300

    add new color to Palette

--- lib/palette.dart
+++ lib/palette.dart
@@ -3,9 +3,11 @@ library paints;
 import 'dart:ui';
 
 import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
 
 class Palette {
   static PaletteEntry white = BasicPalette.white;
   static PaletteEntry black = BasicPalette.black;
   static PaletteEntry blue = PaletteEntry(Color(0xFF0000ff));
+  static PaletteEntry green = PaletteEntry(Color.fromARGB(255, 50, 255, 50));
 }
