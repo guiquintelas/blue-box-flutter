02fb939f7c3250a1e0df44425b38297bf52a38e4
Guilherme Frota <guiquintelas96@gmail.com>
2020-05-06 17:13:12 -0300

    add text_util

---
+++ lib/components/text_util.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class TextUtil {
+  static TextStyle defaultTextStyle =
+      TextStyle(color: Colors.white, fontSize: 25, fontFamily: 'UnitedKingdom');
+
+  static drawText(Canvas canvas, String text, Offset offset) {
+    final textSpan = TextSpan(
+      text: text,
+      style: defaultTextStyle,
+    );
+
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout();
+
+    textPainter.paint(canvas, offset);
+  }
+}
